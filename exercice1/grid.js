8c155d3a45292403b324d10da47de0bb08929d5d
ChamHostile <hamza.aboudou@gmail.com>
2020-10-07 11:10:37 +0200

    feat(randomColor):created random colors with interval for grid

--- exercice1/grid.js
+++ exercice1/grid.js
@@ -7,9 +7,7 @@
  * @return 
  */
 /*
-var myInterval; 
-function interval(){
-    myInterval = setInterval(function(){ gridGenerator(10,10)}, Math.floor(Math.random() * 1 ) +1 );
+
 }*/
 
 var Grid = function Grid(xAxis, yAxis) {
@@ -19,6 +17,12 @@ var Grid = function Grid(xAxis, yAxis) {
 
 Grid.prototype.run = function() {
   this.gridGenerator(this.xAxis, this.yAxis);
+  var tdLength = document.querySelectorAll('td');
+  setInterval(function(){ 
+    for (var i = 0; i<tdLength.length; i++){
+      this.randColor(i);
+    }   
+  }.bind(this), Math.floor(Math.random() * 1 + 1) * 1000);
 }
 
 Grid.prototype.gridGenerator = function gridGenerator(xLength,yLength){
@@ -33,6 +37,9 @@ Grid.prototype.gridGenerator = function gridGenerator(xLength,yLength){
       var td = document.createElement('td');
       
       td.textContent = '0';
+      var color = ['blue', 'green'];
+      var rand = Math.floor(Math.random() * Math.floor(color.length));
+      td.style.cssText = 'background-color:' + color[rand];
       document.querySelector('table').appendChild(td);
     }
 
@@ -40,6 +47,14 @@ Grid.prototype.gridGenerator = function gridGenerator(xLength,yLength){
   }
 
   return this;
+}
+Grid.prototype.randColor = function randColor(cellNum){
+
+  var color = ['blue', 'green'];
+  var rand = Math.floor(Math.random() * Math.floor(color.length));
+  document.querySelectorAll('td')[cellNum].style.backgroundColor = color[rand];
+  return this;
+
 }
 
 var myGrid = new Grid(10, 10);
