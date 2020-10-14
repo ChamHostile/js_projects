a65898f597fb8747570fa2edd3a9c3a16b7921df
ChamHostile <hamza.aboudou@gmail.com>
2020-10-14 21:08:28 +0200

    feat(interactiveMap): added hover and click interaction on map

--- exercice4/map.js
+++ exercice4/map.js
@@ -5,32 +5,109 @@
  * @return 
  */
 
-var req = new XMLHttpRequest();
-req.open("GET", "https://upload.wikimedia.org/wikipedia/commons/8/80/World_map_-_low_resolution.svg");
-req.send();
 
-function renderWorldMap(){
+/**Ex4
+ * WorldMap
+ *@constructor 
+ */
 
-	var obj = document.createElement('object', true);
-  obj.setAttribute('type', 'image/svg+xml');
-  obj.setAttribute('data', 'https://upload.wikimedia.org/wikipedia/commons/8/80/World_map_-_low_resolution.svg');
-  obj.setAttribute('width', 600);     // image size in pixels
-  obj.setAttribute('height', 500);    // set as required
-  obj.addEventListener('load', function() { ; }, false);
-  document.body.appendChild(obj);
-  	}
+var WorldMap = function WorldMap(){
+  this.map;
+  this.active = null; 
+}
 
-function svgHover(){
-	renderWorldMap();
-  var svgItem = document.querySelector('object');
-  svgItem.addEventListener("load", function(){
+/**Ex4
+ * run
+ * 
+ */
+WorldMap.prototype.run = function(){
 
-  	var svgContent = svgItem.contentDocument;
-  	var svgPath = svgContent.getElementById('niger');
-		console.log(svgContent);
-
-  });
+  this.renderWorldMap();
+  this.svgHover();
+  this.svgClick();
 
 }
 
-svgHover();
+/**Ex4
+ * renderWorldMap
+ * 
+ */
+WorldMap.prototype.renderWorldMap = function renderWorldMap(){
+
+  var req = new XMLHttpRequest();
+  req.open("GET", "https://upload.wikimedia.org/wikipedia/commons/8/80/World_map_-_low_resolution.svg", false);
+  req.send();
+  var svg = req.responseXML.getElementsByTagName('svg')[0];
+  document.body.prepend(svg);
+
+}
+
+/**Ex4
+ * svgHover 
+ */
+WorldMap.prototype.svgHover = function svgHover(){
+  this.map = document.querySelector('svg');
+  var countries = this.map.querySelectorAll('path');
+
+  for(var i = 0; i < countries.length; i++){
+    countries[i].addEventListener('mouseover', function(e){
+      if (e.target.id !== this.active){
+        this.style.fill = "blue";
+      };
+    });
+
+    countries[i].addEventListener('mouseout', function(e){
+      if (e.target.id !== this.active){
+        this.style.fill = "black";
+      };
+    });
+
+  }
+}
+
+/**Ex4
+ * svgClick
+ *@return {object} this 
+ */
+WorldMap.prototype.svgClick = function svgClik(){
+
+  this.map = document.querySelector('svg');
+  var countries = this.map.querySelectorAll('path');
+  for(var i = 0; i < countries.length; i++){
+
+    countries[i].addEventListener('click', function(e){
+
+      if (this.active !== this.id){
+        e.target.style.fill = "red";
+        this.active = e.target.id;
+
+        return this;
+      }
+
+      this.active = null;
+      countries[i].style.fill = 'black';
+      return this;
+    });
+
+  }
+}
+
+/**Ex4
+ * caption 
+ */
+
+WorldMap.prototype.caption = function caption(country){
+
+  this.map = document.querySelector('svg');
+  var caption = document.createElement('p');
+
+  caption.textContent = country.id + '';
+
+  this.map.appendChild(caption);
+
+}
+
+
+
+var myMap = new WorldMap();
+myMap.run();
